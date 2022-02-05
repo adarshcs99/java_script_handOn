ddfa6464b65f8e16821c0e974219d7a47cc09e83
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 13:12:08 +0530

    pop used

--- src/first.js
+++ src/first.js
@@ -158,3 +158,7 @@ const myArray = [
 const myArray = [["John", 23], ["cat", 2]];
 myArray.push(["dog", 3]);
 // Only change code below this line
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+// Only change code below this line
