96a0ca96d1edc91ef73fe81367b52719e29d9c83
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 11:41:56 +0530

    Increment a Number with JavaScript

--- src/first.js
+++ src/first.js
@@ -44,3 +44,7 @@ console.log(FCC, fact); // Change this line
 const sum = 10 + 10;
 const difference = 45 - 33;
 const quotient = 66 / 33;
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
