4efd17bbfdca98b84e5236f201e3fdf1de23e5ee
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 12:41:39 +0530

    Find the Length of a String

--- src/first.js
+++ src/first.js
@@ -95,3 +95,9 @@ const myStr = "Hello, our name is " + myName + ", how are you?";
 const someAdjective = "adarsh";
 let myStr = "Learning to code is ";
 myStr+=someAdjective;
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
