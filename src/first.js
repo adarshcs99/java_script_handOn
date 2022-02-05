8abdcabc12cdb5c7b4dc0a4dd3b354ddedfec088
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 11:14:19 +0530

    Understanding Uninitialized Variables

--- src/first.js
+++ src/first.js
@@ -17,3 +17,12 @@ b=a;
 var a = 9;
 var myFirstName = "your name";
 var myLastName = "your name";
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
