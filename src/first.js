f177b5cf6177898fbeaf0898bfb07e713331f6f9
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 14:27:15 +0530

    >,<,= usewd

--- src/first.js
+++ src/first.js
@@ -333,3 +333,16 @@ function testStrictNotEqual(val) {
   }
   
   testStrictNotEqual(17);
+  function testGreaterThan(val) {
+    if (val>100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
