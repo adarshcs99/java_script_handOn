5b259e05165ea3fadb6c169460e8333f42b7f4f0
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 12:29:32 +0530

    Concatenating Strings with the Plus Equals Operator

--- src/first.js
+++ src/first.js
@@ -86,3 +86,5 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Ch
 const myStr = "This is the start." + " This is the end."; // Change this line
 const myStr="FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 const myStr = "This is the start." + " This is the end."; // Change this line
+let myStr="This is the first sentence.";
+myStr+=" This is the second sentence."
