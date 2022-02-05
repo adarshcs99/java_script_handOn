d7e8adcc128faa02167e90c311f4a2f5dbfa7bd6
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 13:09:08 +0530

    push method used

--- src/first.js
+++ src/first.js
@@ -154,3 +154,7 @@ const myArray = [
   ];
   
   const myData = myArray[2][1];
+  // Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// Only change code below this line
