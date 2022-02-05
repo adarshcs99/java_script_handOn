2f54dbe4b8c9b9f3450b258a5575fbd03a9b1916
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-05 13:05:18 +0530

    Modifed array

--- src/first.js
+++ src/first.js
@@ -142,3 +142,7 @@ const myArray = ["aadrsh"];
 const myArray = [["Bulls", 23], ["White Sox", 45]];
 const myArray = [50, 60, 70];
 let myData  = myArray[0];
+// Setup
+const myArray = [18, 64, 99];
+myArray[0] = 45;
+// Only change code below this line
