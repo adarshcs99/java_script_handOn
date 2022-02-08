bf9ccd3abf7ecfdd265906c4082d55693a9ce7e0
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-08 10:29:44 +0530

    Comparisons with the Logical And Operator

--- src/first.js
+++ src/first.js
@@ -399,3 +399,15 @@ function testStrictNotEqual(val) {
   }
   
   testLessOrEqual(10);
+  function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val>24 && val<51) {
+     return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
